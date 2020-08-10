a36fcf8e227cf0b4a0db2b87374ba983893b6c9e
justinlautner <jlautner@protonmail.com>
2020-08-09 22:30:22 -0500

    refresh repository to reflect .gitignore

--- Hangman-Game/Program.cs
+++ Hangman-Game/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Net.Http;
 using Gtk;
 
 namespace HangmanGame
 {
     class MainClass
     {
+        
+        HttpClient client = new HttpClient();
+            
         public static void Main(string[] args)
         {
             new GameEngine();
+
             Application.Init();
             MainWindow win = new MainWindow();
             win.Show();
